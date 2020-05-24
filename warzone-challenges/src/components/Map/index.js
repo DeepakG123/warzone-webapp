6ac5ea86b20108731a36b6df15518c80e03fd857
Deepak Goel <goel.deepak9@gmail.com>
2020-05-24 19:39:33 -0400

    first commit

---
+++ warzone-challenges/src/components/Map/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import L from 'leaflet';
+import 'leaflet/dist/leaflet.css';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+   width: ${props => props.width};
+   height: ${props => props.height};
+`;
+
+
+ export default class Map extends React.Component{
+   componentDidMount(){
+     var bounds = [[50,-30], [-45,100]];
+     this.map = L.map('map', {
+       center:[5,37.5],
+       zoom:4,
+       maxZoom:8,
+       minZoom:4,
+       maxBounds:[ [ 50, -30 ], [ -45, 100 ] ]
+     });
+     //this.map.setMinZoom(this.map.getBoundsZoom([[0,85],[0,85]], true) );
+     L.imageOverlay('https://assets.rockpapershotgun.com/images/2020/03/Verdansk-3k-1212x1212.jpg', bounds).addTo(this.map);
+     var sol = L.latLng([ 10, 10 ]);
+     L.marker(sol).addTo(this.map);
+   }
+
+   render(){
+     return <Wrapper width="1212px" height="1212px" id="map" />
+   }
+ }
